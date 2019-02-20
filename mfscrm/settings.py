f2f47ead3cbedc164d020973093a1ccfc2afafd1
jdfranco97 <jdfranco@unomaha.edu>
2019-02-20 16:13:07 -0600

    Static file location fix

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/crm/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/home'
